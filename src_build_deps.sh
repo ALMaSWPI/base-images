3b4293a5b7b44e9ccdb47e45071bc7ca1ee23bfa
Duc Doan <dtbpkmte@gmail.com>
2024-04-29 12:23:01 -0400

    feat: Update libsockcanpp and serial dependencies

--- src_build_deps.sh
+++ src_build_deps.sh
@@ -5,14 +5,14 @@ set -e
 echo "Target platform: $1"
 
 # Build and install libsockcanpp
-git clone https://github.com/SimonCahill/libsockcanpp.git
+git clone https://github.com/ALMaSWPI/libsockcanpp.git
 cd libsockcanpp
 cmake --toolchain toolchains/$1-toolchain.cmake -DBUILD_SHARED_LIBS=ON -Bbuild
 cmake --build build --parallel --target install
 echo "libsockcanpp installed!"
 
 # Build and install Serial
-git clone https://github.com/wpi-huron/serial.git
+git clone https://github.com/ALMaSWPI/serial.git
 cd serial
 cmake -DBUILD_SHARED_LIBS=ON -Bbuild
 cmake --build build --parallel --target install
